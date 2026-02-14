7e22fe12c07c95637c4ba4374f7e78c23903db8c
Manu Paul <166366565+manu-paul20@users.noreply.github.com>
2026-02-14 18:12:41 +0530

    gave some final touch

--- app/src/main/java/com/manu/newsapplication/constants/MyConstants.kt
+++ app/src/main/java/com/manu/newsapplication/constants/MyConstants.kt
@@ -1,6 +1,6 @@
 package com.manu.newsapplication.constants
 
 object MyConstants {
-    val API_KEY = "pub_2bb18ec7e87c4ce4a62adfd8896cd863"
+    val API_KEY = "YOUR API KEY"
     val API_BASE_URL = "https://newsdata.io"
 }
